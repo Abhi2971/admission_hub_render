b133d36a8ca88e6a21cab6edfc540aab1d8a2a4b
asmitashelke2002 <shelkeasmita218@gmail.com>
2026-04-23 19:45:21 +0530

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=port,
+        allow_unsafe_werkzeug=True
+    )
